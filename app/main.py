963a06e56381ef23fb1a7ff70b621c38ffeb1ec0
Nikolay <nchernov@gmail.com>
2022-05-17 17:38:16 +0300

    test refactoring

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ from .database import engine
 from app import posts, user, auth
 from .config import settings
 
-models.Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
 app.include_router(posts.router)
 app.include_router(user.router)
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,41 +1,7 @@
-import pytest
-from typing import Generator
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import Session
 
-from app.main import app
 from app.models import User
-from app.database import Base, get_db
-
-
-@pytest.fixture()
-def client() -> Generator:
-
-    with TestClient(app) as c:
-        yield c
-
-
-@pytest.fixture()
-def db() -> Generator:
-    # SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
-    SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-    )
-    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    with TestingSessionLocal() as db:
-
-        def override_get_db() -> Generator:
-            yield db
-
-        app.dependency_overrides[get_db] = override_get_db
-
-        yield db
-        Base.metadata.drop_all(bind=engine)
 
 
 def test_auth(client: TestClient, db: Session):
